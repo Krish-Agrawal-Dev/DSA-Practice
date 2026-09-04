e25cd7d83e81ac85acbaa2ebcc8642f6e1f6a9ea
Krish Agrawal <krish.agrawal238@gmail.com>
2026-09-04 10:33:26 +0530

    Implement containsDuplicate function in C++

---
+++ Hashing/LC-217-Contains-Duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_set<int> s;
+        
+        for(auto x : nums){
+
+            if (s.count(x))
+            {
+                return true;
+
+            } else {
+
+                s.insert(x);
+            }
+        }
+
+        return false;
+        
+    }
+};
